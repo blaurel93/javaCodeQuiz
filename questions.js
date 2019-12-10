ac352465ea7c3e39b59e04e016aeec61e6aad419
Blaise <ifollowjre@gmail.com>
2019-12-09 17:57:07 -0700

    started making the basic look of where all the buttons are going to be held and the time is going to be displayed

---
+++ questions.js
@@ -0,0 +1,28 @@
+// Questions // 
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "This is a question about javascript",
+        choices: ["its cold", "its hot", "its medium", "its soft"],
+        answer: "its cold"
+    },
+    {
+        title: "This is another question about javaScript",
+        choices: ["its these {}", "its these []", "its these ||", "its these()"],
+        answer: "its these {}"
+    },
+    {
+        title: "This is the last question about javaScript",
+        choices: ["might be this one", "could be this one", "defiantly not this one", "this one"],
+        answer: "might be this one"
+    }
+];
