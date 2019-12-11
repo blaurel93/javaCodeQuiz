04d591935ad2550cd46a8456ed43c2cc2befc79f
Blaise <ifollowjre@gmail.com>
2019-12-11 10:44:17 -0700

    restarting again...

--- jumpin.js
+++ jumpin.js
@@ -1,16 +1,9 @@
 
 $(document).ready(function () {
-    $("#extraControls").hide();
-
-    var highScores = $("#highScores");
-    var timer = $("#timer");
-    var hideShow = $("#hideShow");
+ 
 
 
-    $("#startButton").click(function () {
-        $("#extraControls").show();
-        $("#startButton").hide();
-    });
 
 
+    
 });
